824606e64dee03df334d5ec11c1ee56ec5f1a909
Olivier Van hamme <nicefiction@gmail.com>
2022-04-13 18:51:54 +0200

    Initial Commit

---
+++ iExpenseChallenge/iExpenseChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseChallengeApp.swift
+//  iExpenseChallenge
+//
+//  Created by Olivier Van hamme on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
