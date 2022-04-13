046f0dd5f010249e4213c05d8cc1fc5aa543b860
Olivier Van hamme <nicefiction@gmail.com>
2022-04-13 21:06:18 +0200

    Completes Challenge 38.1: Use the user’s preferred currency, rather than always using US dollars.

--- iExpenseChallenge/ContentView.swift
+++ iExpenseChallenge/ContentView.swift
@@ -1,21 +1,78 @@
-//
-//  ContentView.swift
-//  iExpenseChallenge
-//
-//  Created by Olivier Van hamme on 13/04/2022.
-//
+/// SOURCE:
+/// https://www.hackingwithswift.com/books/ios-swiftui/iexpense-wrap-up
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    // MARK: - PROPERTY WRAPPERS
+    @StateObject var expenses = Expenses.init()
+    
+    @State private var isShowingAddExpenseItemView: Bool = false
+    
+    
+    
+    // MARK: - PROPERTIES
+    // MARK: - INITIALIZERS
+    // MARK: - COMPUTED PROPERTIES
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List {
+                ForEach(expenses.items) { (eachItem: ExpenseItem) in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("\(eachItem.name)")
+                                .font(.headline)
+                            Text("\(eachItem.type)")
+                                .font(.subheadline.smallCaps())
+                                .foregroundColor(.secondary)
+                        }
+                        Spacer()
+                        Text("\(eachItem.price, format: .currency(code: "EUR"))")
+                            .font(.title)
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle(Text("iExpense"))
+            .toolbar {
+                Button {
+                    isShowingAddExpenseItemView.toggle()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .font(.title)
+                }
+            }
+            .sheet(isPresented: $isShowingAddExpenseItemView) {
+                AddExpenseItemView(expenses: expenses)
+            }
+        }
     }
+    
+    
+    
+    // MARK: - METHODS
+    func removeItems(at offsets: IndexSet)
+    -> Void {
+        
+        expenses.items.remove(atOffsets: offsets)
+    }
+    // MARK: - HELPERMETHODS
 }
 
+
+
+
+
+// MARK: - PREVIEWS
+
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         ContentView()
     }
 }
